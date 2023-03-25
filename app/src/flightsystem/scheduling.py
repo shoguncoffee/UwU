b958ed402b3a5dbdaf8d8398b694173fbabf0b8c
Sun Chokun <63220735+shoguncoffee@users.noreply.github.com>
2023-03-25 23:08:39 +0700

    feat: update scheduling

--- app/src/flightsystem/scheduling.py
+++ app/src/flightsystem/scheduling.py
@@ -2,18 +2,42 @@
 """
 from __future__ import annotations
 from base import *
-
+if TYPE_CHECKING:
+    from src import Flight, ScheduleCatalog
     
-@dataclass
+
 class FlightScheduling:
-    ...
+    __plans: set[Flight] #type: ignore
+    __advance_days: int = 365 #type: ignore
+    
+    @classmethod
+    def add(self, plan: FlightPlan):
+        self.__plans.add(plan)
     
     
 @dataclass    
 class FlightPlan:
-    ...
+    __flight: Flight # type: ignore
+    __start_date: date # type: ignore
+    __end_date: Optional[date] = None # type: ignore
+    __exception: Deviation # type: ignore
     
+    @property
+    def duration(self):
+        """
+        if not have end date, return None
+        """
+        if self.__end_date is not None:
+            return self.__end_date - self.__start_date
+        
+        return None
     
+    def scheduled(self):
+        FlightScheduling.add(self)
+    
+
 @dataclass    
 class Deviation:
-    ...
+    __weekdays: set[int] # type: ignore
+    __months: set[int] # type: ignore
+    __dates: set[date] # type: ignore
