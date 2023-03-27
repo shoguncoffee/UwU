c00c0a869417ffd675256bf331a85d746b498a94
Sun Chokun <63220735+shoguncoffee@users.noreply.github.com>
2023-03-27 23:26:13 +0700

    chore: change name and dir

---
+++ app/src/airline.py
@@ -0,0 +1,65 @@
+"""
+"""
+from __future__ import annotations
+from base import *
+from fastapi import FastAPI
+
+from src import *
+from src.flightsystem import Flight
+"""
+class Airline(Singleton):
+    name = 'Qatar Airways'
+    designator = 'QR'
+    _instance: Airline
+    
+    def __init__(self):
+        self.__accounts: set[Account] = set()
+        
+        self.init_aircraft()
+        self.init_airport()
+    
+    def init_aircraft(self):
+        return
+        with open('app/data/aircraft.json') as f:
+            data = ...
+    
+    def init_airport(self):
+        return
+        with open('app/data/airport.json') as f:
+            data = ...
+            
+    def load(self):
+        self._instance
+    
+    @classmethod
+    def create_booking(cls,
+        creator: Customer,
+        journey: list[Trip],
+        passengers: list[PassengerDetails],
+        contact: ContactInformation
+    ):
+        reserve = [
+            SeatReservation(passenger, None) for passenger in passengers
+        ]
+        for trip in journey:
+            for flight in trip.flight:
+                
+                FlightReservation(
+                    trip.travel_class,
+                    ,
+                    flight,
+                    reserve
+                )
+        
+        return Booking(
+            creator,
+            passengers,
+            ,
+            contact,
+        )
+        
+    @classmethod 
+    def asd(cls):
+        cls._instance
+        
+"""
