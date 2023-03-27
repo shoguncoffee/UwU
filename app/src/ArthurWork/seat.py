78790f75b3ba7b678ee8cb5ceb7143766b53088d
Tanawat Laong <110480443+kodpleum@users.noreply.github.com>
2023-03-27 16:26:49 +0700

    #commit2#init_class_work

--- app/src/ArthurWork/seat.py
+++ app/src/ArthurWork/seat.py
@@ -1,2 +1,8 @@
 class Seat:
-    ...
+
+    def __init__(self, row, column, type, passenger, description):
+        self.__row = row
+        self.__column = column
+        self.__type = type
+        self.__passenger = []
+        self.__description = description
