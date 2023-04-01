d5a133319b6cd85245ce21df93ea7aa70394aae8
Sun Chokun <63220735+shoguncoffee@users.noreply.github.com>
2023-04-01 10:22:58 +0700

    feat: update import, mod method and constructor

--- app/src/booking_related/booking.py
+++ app/src/booking_related/booking.py
@@ -1,31 +1,28 @@
 from __future__ import annotations
 from ..base import *
 if TYPE_CHECKING:
-    from .customer import Customer
-    from .passenger_detail import PassengerDetails
-    from .contact_information import ContactInformation
-    from ..K import Payment
-    from ..ArthurWork.flight_itinerary import Trip
+    from app.src import *
 from dataclasses import InitVar
-from .flight_reservation import FlightReservation
+from .reservation import FlightReservation
 
 @dataclass(slots=True, unsafe_hash=True)
 class Booking: #(HasReference):
     __datetime: datetime = field(init=False, default_factory=datetime.now)
-    __reservation: tuple[FlightReservation, ...] = field(init=False, hash=False) # type: ignore
     # __reference: Optional[UUID] = reference or self.generate_reference() # type: ignore
     
-    __journey: InitVar[list[Trip]] # type: ignore
     __creator: Customer = field(hash=False) # type: ignore
-    __passenger: tuple[PassengerDetails, ...] = field(hash=False) # type: ignore
+    __journey: InitVar[list[Trip]] # type: ignore
     __contactinfo: ContactInformation = field(hash=False) # type: ignore
-    __payment: Optional[Payment] = field(init=False, hash=False, default=None) # type: ignore
-    __status: BookingStatus = field(init=False, hash=False, default=BookingStatus.INCOMPLETE) # type: ignore
+    __passenger: tuple[PassengerDetails, ...] = field(hash=False) # type: ignore
+    
+    __reservation: tuple[FlightReservation, ...] = field(init=False, hash=False)
+    __payment: Optional[Payment] = field(init=False, hash=False, default=None)
+    __status: BookingStatus = field(init=False, hash=False, default=BookingStatus.INCOMPLETE)
 
     def __post_init__(self, journey: list[Trip]):
         self.__reservation = tuple(
             FlightReservation(flight, trip.travel_class, self) 
-            for trip in journey for flight in trip.itinerary.flights
+            for trip in journey for flight in trip.flights
         )
 
     @property
@@ -62,12 +59,11 @@ class Booking: #(HasReference):
         return self.status == BookingStatus.COMPLETED
     
     @property
-    def is_connecting_flight(self):
-        return len(self.reservation) > 1
-    
-    @property
-    def passenger_number(self):
+    def pax(self):
         return len(self.passenger)
     
     def cancel(self):
         self.__status = BookingStatus.CANCELLED
+        
+    def pending(self):
+        self.__status = BookingStatus.PENDING
