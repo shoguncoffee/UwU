d6f3ff90b6187e0ad15a00004033ff5cfcfce093
Sun Chokun <63220735+shoguncoffee@users.noreply.github.com>
2023-04-03 04:29:40 +0700

    feat: add daterange method

--- app/src/base.py
+++ app/src/base.py
@@ -8,6 +8,7 @@ class HasReference(ABC):
     """
     base on uuid4
     """
+    # reference: Optional[UUID]
     @abstractmethod
     def get_reference_pool(self) -> Collection:
         pass 
@@ -17,3 +18,13 @@ class HasReference(ABC):
         while 1:
             if (ref := uuid4()) not in pool:
                 return ref
+
+def daterange(days: int, start: Optional[date] = None):
+    """
+    if start is None, start = date.today()
+    
+    *include start and end
+    """
+    start = start or date.today()
+    for i in range(days + 1):
+        yield start + timedelta(i)
