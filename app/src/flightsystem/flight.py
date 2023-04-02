9a065b1affec0a2429c6d87df738cac18fd6a453
Sun Chokun <63220735+shoguncoffee@users.noreply.github.com>
2023-04-03 04:27:09 +0700

    feat: add booking method

--- app/src/flightsystem/flight.py
+++ app/src/flightsystem/flight.py
@@ -1,19 +1,20 @@
-"""
-take care of flight system
-"""
 from __future__ import annotations
 from ..base import *
+
 if TYPE_CHECKING:
     from app.src import *     
 
-@dataclass(slots=True, frozen=True)
-class Flight: #(HasReference):
+
+@dataclass(slots=True)
+class Flight:
+    """
+    A flight template for FlightInstance
+    """
     __designator: str # type: ignore
     __departure: time # type: ignore
     __arrival: time # type: ignore
     __origin: Airport # type: ignore
     __destination: Airport # type: ignore
-    # __reference: Optional[UUID] = None # type: ignore
     
     @property
     def designator(self):
@@ -37,26 +38,25 @@ class Flight: #(HasReference):
     
     @property
     def duration(self) -> timedelta:
-        crossday = timedelta(self.__arrival < self.__departure)
-        d2 = datetime.combine(date.min + crossday, self.__arrival)
-        d1 = datetime.combine(date.min, self.__departure)
+        crossday = timedelta(self.arrival < self.departure)
+        d2 = datetime.combine(date.min + crossday, self.arrival)
+        d1 = datetime.combine(date.min, self.departure)
         return d2 - d1
-    
-    # @classmethod
-    # def generate_reference(cls):
-    #     return 
 
 
-@dataclass(slots=True, unsafe_hash=True)
+@dataclass(slots=True)
 class FlightInstance:
-    __flight: Flight # type: ignore
+    """
+    A derivative of Flight
+    """
     __date: date # type: ignore
+    __flight: Flight # type: ignore
     __aircraft: Aircraft # type: ignore
     
-    __base_price: float = field(hash=False) # type: ignore
+    __base_fare: float # type: ignore
     
-    __booking_record: set[FlightReservation] = field(init=False, hash=False, default_factory=set)
-    __status: FlightStatus = field(init=False, hash=False, default=FlightStatus.SCHEDULED)
+    __booking_record: set[FlightReservation] = field(init=False, default_factory=set)
+    __status: FlightStatus = field(init=False, default=FlightStatus.SCHEDULED)
     
     @property
     def flight(self):
@@ -79,8 +79,20 @@ class FlightInstance:
         return self.__status
     
     @property
+    def base_fare(self):
+        return self.__base_fare
+    
+    @property
     def designator(self):
         return self.flight.designator
+    
+    @property
+    def origin(self):
+        return self.flight.origin
+    
+    @property
+    def destination(self):
+        return self.flight.destination
     
     @property
     def all_travel_class(self):
@@ -123,9 +135,21 @@ class FlightInstance:
         }
 
     def bookable(self, travel_class: TravelClass, pax: int):
+        """
+        if specified travel class of this FlightInstance is bookable for pax
+        """
         free = self.get_seats_of(travel_class)
         occupied = self.get_occupied_of(travel_class)
         return len(free) - len(occupied) >= pax
+    
+    def bookingable(self, pax: int):
+        """
+        if any travel class of this FlightInstance bookable for pax
+        """
+        return any(
+            self.bookable(travel_class, pax) 
+            for travel_class in self.all_travel_class
+        )
     
     def booked(self, reservation: FlightReservation):
         self.booking_record.add(reservation)
