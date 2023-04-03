3c70a7afd63ab28bde8fbacbf52131f1473ff4c7
Sun Chokun <63220735+shoguncoffee@users.noreply.github.com>
2023-04-03 21:35:50 +0700

    feat: update enum contanst

--- app/src/constants.py
+++ app/src/constants.py
@@ -4,22 +4,22 @@ enum constants
 from enum import Enum as _Enum
 
 FlightStatus = _Enum(
-    'FlightStatus', 'SCHEDULED CANCELLED DELAYED IN_FLIGHT ARRIVED'
+    'FlightStatus', 'SCHEDULED CANCELLED IN_FLIGHT ARRIVED'
 )
 PassengerType = _Enum(
     'PassengerType', 'ADULT CHILD INFANT'
 )
 TravelClass = _Enum(
-    'TravelClass', 'Economy Bussiness First'
+    'TravelClass', 'ECONOMY BUSSINESS FIRST'
 )
 AccountStatus = _Enum(
-    'AccountStatus', 'PENDING ACTIVE SUSPENDED'
+    'AccountStatus', 'PENDING ACTIVE SUSPENDED, INACTIVE'
 )
 BookingStatus = _Enum(
     'BookingStatus', 'FAILED INCOMPLETE PENDING COMPLETED CANCELLED'
 )
 SeatType = _Enum(
-    'SeatType', 'STANDARD'
+    'SeatType', 'LEGROOM WINDOW AISLE COMMON'
 )
 GenderType = _Enum(
     'GenderType', 'MALE FEMALE'
