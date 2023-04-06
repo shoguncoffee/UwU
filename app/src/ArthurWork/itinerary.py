0630c038bd48275ef3b599940a61a853f0ee70c0
Sun Chokun <63220735+shoguncoffee@users.noreply.github.com>
2023-04-06 19:19:35 +0700

    fix: correct bookable func add docs and min_fare func

--- app/src/ArthurWork/itinerary.py
+++ app/src/ArthurWork/itinerary.py
@@ -5,38 +5,48 @@ if TYPE_CHECKING:
     from app.src import FlightInstance
 
 
-@dataclass(init=False, slots=True, unsafe_hash=True)
+@dataclass(init=False, slots=True)
 class FlightItinerary:
     """
-    collection of flight
+    collection of FlightInstance, represent connecting flights,
+    provide some useful methods to get information from group of flights,
+    intended to be used for summarizing information to user
     """
-    __flights: tuple[FlightInstance, ...]
+    __instances: tuple[FlightInstance, ...]
     
     def __init__(self, *flight: FlightInstance):
-        self.__flights = flight
+        self.__instances = flight
     
     @property
     def flights(self):
-        return self.__flights
+        return self.__instances
     
     def choice(self, cls: TravelClass):
         return Trip(self, cls)
     
     def bookable(self, pax: int):
         """
-        check if this itinerary is bookable by any travel class
+        check if this itinerary is bookable by any travel class for pax (number of passenger)
         """
         return all(
-            flight.bookable(travel_class, pax) 
+            any(
+                flight.bookable(travel_class, pax) 
+                for travel_class in flight.all_travel_class
+            )
             for flight in self.flights
-            for travel_class in flight.all_travel_class
+            
+        )
+        
+    def minimal_fare(self):
+        return sum(
+            instance.base_fare for instance in self.flights
         )
 
 
 @dataclass(slots=True, frozen=True)
 class Trip:
     """
-    trip segment, just a ...
+    trip segment, a FlightItinerary with a specific travel class
     """
     __itinerary: FlightItinerary # type: ignore
     __travel_class: TravelClass # type: ignore
@@ -51,7 +61,7 @@ class Trip:
     
     def bookable(self, pax: int):
         """
-        check if this trip is bookable
+        check if this trip is bookable (specific travel class)
         """
         return all(
             flight.bookable(self.travel_class, pax) 
