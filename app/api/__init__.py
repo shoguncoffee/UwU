23d5248903a75918d99b6189e64482a82a70c16c
Tanaphat <65010421@kmitl.ac.th>
2023-04-17 19:09:20 +0700

    <fix>:delete some comment

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -21,19 +21,3 @@ async def delete_aircraft(model:str) -> dict:
             aircrafts.remove(aircraft)
             return {"Data": "Deleted"}
     return {"Data": "Not Found"}
-
-'''from src.K.payment import Payment
-
-payment = [Payment]
-
-@app.get("/payment",tags=["payment"])
-async def get_payment() -> dict:
-    return {"Data": payment}
-
-@app.post("/payment",tags=["payment"])
-async def add_payment(transaction_id:str,payment_time:str,status:str) -> dict:
-    for payment in payment:
-        if transaction_id in payment.transaction_id:
-            payment.append(payment)
-            return {"Data": "Added"}'''
-    
