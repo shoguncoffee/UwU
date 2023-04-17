4d59fc0f8e1b372c71dc901a19e39d8456a68e73
Sun Chokun <63220735+shoguncoffee@users.noreply.github.com>
2023-04-18 00:46:03 +0700

    feat: add import constant

--- app/base.py
+++ app/base.py
@@ -2,29 +2,33 @@
 fancy variables and functions
 """
 from __future__ import annotations
+
 # from .utils.checking import *
 from .utils.packages import *
+from .constants import *
+
 if TYPE_CHECKING:
     T = TypeVar('T')
     U = TypeVar('U')
     
-def daterange(days: int, start: Optional[date] = None):
+    
+def daterange(days: int, start: Optional[dt.date] = None):
     """
-    if start is None, start = date.today()
+    if start is None, start = dt.date.today()
     
     *include start and end
     """
-    start = start or date.today()
+    start = start or dt.date.today()
     for i in range(days + 1):
-        yield start + timedelta(i)
+        yield start + dt.timedelta(i)
         
 
-def difference_time(start: time, end: time):
+def difference_time(start: dt.time, end: dt.time):
     """
     calculate the difference between two time objects,
     if end time is earlier than start time, it will be considered as the next day
     """
-    crossday = timedelta(end < start)
-    d2 = datetime.combine(date.min + crossday, end)
-    d1 = datetime.combine(date.min, start)
+    crossday = dt.timedelta(end < start)
+    d2 = dt.datetime.combine(dt.date.min + crossday, end)
+    d1 = dt.datetime.combine(dt.date.min, start)
     return d2 - d1
