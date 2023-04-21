ad763d78f277eb03955d1ed2c498511fd83c11b1
Sun Chokun <63220735+shoguncoffee@users.noreply.github.com>
2023-04-21 14:59:07 +0700

    refactor: improve generate method and remove unnecessary method

--- app/src/aircraft.py
+++ app/src/aircraft.py
@@ -5,110 +5,36 @@ from string import ascii_uppercase
 
 
 @dataclass(slots=True, frozen=True)
-class Aircraft:
-    __model: str # type: ignore
-    __desks: tuple[Desk, ...] # type: ignore
+class Seat:
+    __row: int # type: ignore
+    __column: int # type: ignore
+    __number: str # type: ignore
+    __type: SeatType # type: ignore
+    __descriptions: list[str] = field(hash=False, default_factory=list) # type: ignore
     
     @property
-    def model(self):
-        return self.__model
+    def row(self):
+        return self.__row
     
     @property
-    def desks(self):
-        return self.__desks
+    def number(self):
+        return self.__number
     
     @property
-    def all_cabins(self):
-        return {
-            cabin for desk in self.desks
-            for cabin in desk.cabins
-        }
-        
+    def column(self):
+        return self.__column
+    
     @property
-    def all_seats(self):
-        """
-        get all passenger's seat in aircraft
-        """
-        return {
-            seat for desk in self.desks
-            for seat in desk.get_seats()
-        }
-        
-    def get_seats_of(self, travel_class: TravelClass):
-        """
-        get all passenger's seat that match travel_class in aircraft
-        """
-        return {
-            seat for desk in self.desks
-            for seat in desk.get_seats_of(travel_class)
-        }
-        
-    def get_seat(self, number: str):
-        """
-        get seat by row and column
-        """
-        for seat in self.all_seats:
-            if seat.number == number:
-                return seat
-        raise KeyError
-        
+    def type(self):
+        return self.__type
+    
+    @property
+    def descriptions(self):
+        return self.__descriptions
+
 
 @dataclass(slots=True, frozen=True)
-class Desk:
-    __cabins: tuple[CabinLayout, ...] # type: ignore
-    
-    @property
-    def cabins(self):
-        return self.__cabins
-
-    def get_seats(self):
-        """
-        get all passenger's seat in this desk
-        """
-        return {
-            seat for cabin in self.cabins
-            for seat in cabin.seats
-        }
-    
-    def get_cabins_of(self, travel_class: TravelClass):
-        """
-        get all cabin that match travel_class in this desk
-        """
-        return {
-            cabin for cabin in self.cabins 
-            if cabin.travel_class == travel_class
-        }
-    
-    def get_seats_of(self, travel_class: TravelClass):
-        """
-        get all passenger's seat that match travel_class in this desk
-        """
-        return {
-            seat for cabin in self.get_cabins_of(travel_class)
-            for seat in cabin.seats
-        }
-        
-    @classmethod
-    def generate(cls,
-        *layouts: tuple[
-            TravelClass, 
-            Sequence[
-                tuple[int, Sequence[int]]
-            ]              
-        ]
-    ):
-        return cls(tuple(
-            CabinLayout.generate(
-                travel_class, *layout, 
-                initial_row=sum(
-                    sum(q[0] for q in k[1]) for k in layouts[:i]
-                )
-            ) for i, (travel_class, layout) in enumerate(layouts)
-        ))
-        
-
-@dataclass(slots=True, frozen=True)
-class CabinLayout:
+class Cabin:
     __travel_class: TravelClass # type: ignore
     __seats: frozenset[Seat] = field(repr=False) # type: ignore
     
@@ -123,76 +49,131 @@ class CabinLayout:
     @classmethod
     def generate(cls, 
         travel_class: TravelClass,
+        initial_row: int,
         *layout: tuple[int, Sequence[int]],
-        initial_row: int = 1,
     ):
         """
-        generate a CabinLayout
+        generate a `Cabin`, for use in spawn.py
         """
-        l = []
-        for lenght, config in layout:
-            width = sum(config)
-            aisle = {
-                sum(config[:i]) - k
-                for i in range(1, len(config)) 
-                for k in range(2)
+        seats = []
+        for lenght, column_config in layout:
+            aisle_columns = {
+                sum(column_config[:column]) - i
+                for column in range(1, len(column_config)) 
+                for i in range(2)
             }
+            width = sum(column_config)
             for row, column in product(
-                range(1, lenght+1), range(1, width+1)
+                range(1, lenght + 1), range(1, width + 1)
             ):
                 type = SeatType.COMMON
                 info = []
+                
+                if column in aisle_columns:
+                    type = SeatType.AISLE
+                    info.append('Seat has direct access to the aisle')
+                    
+                elif column in (1, width):
+                    type = SeatType.WINDOW
+                    info.append('Seat has access to the window')
                 
                 if row == 1:
                     type = SeatType.LEGROOM
                     info.append('Seat has more legroom')
                 
-                if column in aisle:
-                    info.append('Seat has direct access to the aisle')
-                    
-                if column in (1, width):
-                    info.append('Seat has access to the window')
-                
-                letter = ascii_uppercase[column-1]                
-                l.append(Seat(
-                    initial_row + row, 
-                    column, 
-                    letter, 
-                    type, 
-                    info
+                absolute_row = initial_row + row
+                seats.append(
+                    Seat(
+                        absolute_row, 
+                        column, 
+                        ascii_uppercase[column-1] + str(absolute_row), 
+                        type, 
+                        info
                     )
                 )
-        return cls(travel_class, frozenset(l))
+        return cls(travel_class, frozenset(seats))
 
 
-@dataclass(slots=True, frozen=True)
-class Seat:
-    __row: int # type: ignore
-    __column: int # type: ignore
-    __letter: str # type: ignore number?
-    __type: SeatType # type: ignore
-    __description: list[str] = field(hash=False, default_factory=list) # type: ignore
+class Desk(tuple[Cabin, ...]):
+    def get_cabins_of(self, travel_class: TravelClass):
+        """
+        get all cabins in this desk that match with a travel class
+        """
+        for cabin in self :
+            if cabin.travel_class is travel_class:
+                yield cabin
+    
+    @classmethod
+    def generate(cls,
+        *cabin_layout: tuple[
+            TravelClass, Sequence[tuple[int, Sequence[int]]]
+        ]
+    ):
+        """
+        generate a `Desk`, for use in spawn.py
+        """
+        cabins = []
+        row = 1
+        for travel_class, layout in cabin_layout:
+            cabins.append(
+                Cabin.generate(travel_class, row, *layout)
+            )
+            row += sum(lenght for lenght, _ in layout) 
+            
+        return cls(cabins)
+        
+
+@dataclass(slots=True)
+class Aircraft:
+    __model: str # type: ignore
+    __desks: tuple[Desk, ...] # type: ignore
     
     @property
-    def row(self):
-        return self.__row
+    def model(self):
+        return self.__model
     
     @property
-    def letter(self):
-        return self.__letter
+    def desks(self):
+        return self.__desks
     
     @property
-    def column(self):
-        return self.__column
+    def all_cabins(self):
+        for desk in self.desks:
+            yield from desk
     
     @property
-    def type(self):
-        return self.__type
-    
-    @property
-    def description(self):
-        return self.__description
-    
-    @property
-    def number(self):
-        return str(self.row) + self.letter
+    def all_seats(self):
+        """
+        get all passenger's seat in aircraft
+        """
+        return {
+            seat for cabin in self.all_cabins
+            for seat in cabin.seats
+        }
+        
+    def get_cabins_of(self, travel_class: TravelClass):
+        """
+        get all cabin that match travel_class in this desk
+        """
+        for cabin in self.all_cabins:
+            if cabin.travel_class is travel_class:
+                yield cabin
+        
+    def get_seats_of(self, travel_class: TravelClass):
+        """
+        get all passenger's seat whch in specifiy travel_class in aircraft
+        """
+        return {
+            seat for cabin in self.get_cabins_of(travel_class)
+            for seat in cabin.seats
+        }
+        
+    def get_seat(self, number: str):
+        """
+        get seat by number (designator)
+        """
+        for seat in self.all_seats:
+            if seat.number == number:
+                return seat
+            
+        raise KeyError
