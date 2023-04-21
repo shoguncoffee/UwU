b32d9248a8da9a445b13fe12d515296b98fca4fb
Sun Chokun <63220735+shoguncoffee@users.noreply.github.com>
2023-04-21 15:09:49 +0700

    feat, refactor: add FlightClass, rewrite method, move Fare to fare.py docs: add extra docstring

--- app/src/flight.py
+++ app/src/flight.py
@@ -1,14 +1,17 @@
 from __future__ import annotations
 from .base import *
 
+from dataclasses import InitVar
 if TYPE_CHECKING:
-    from app.src import FlightReservation, Aircraft, Airport, PassengerDetails, Seat, Pax
+    from app.src import *
 
 
 @dataclass(slots=True)
 class Flight:
     """
-    ### A flight template for FlightInstance
+    #### A flight template for FlightInstance, represent a flight
+        - designator: `str`
+            - aka "flight number" in colloquial term
     """
     __designator: str # type: ignore
     __departure: dt.time # type: ignore
@@ -41,80 +44,38 @@ class Flight:
         return difference_time(self.departure, self.arrival)
 
 
-@dataclass(slots=True, frozen=True)
-class Fare:
-    __travel_class: TravelClass # type: ignore
-    __passenger_fare: list[tuple[PassengerType, int]] # type: ignore
-    __seat_fare: list[tuple[SeatType, int]] # type: ignore
-
-    @property
-    def travel_class(self):
-        return self.__travel_class
-    
-    def get_passenger_price(self, passenger_type: PassengerType):
-        for type, price in self.__passenger_fare:
-            if type is passenger_type:
-                return price
-        raise KeyError
-    
-    def set_passenger_price(self, passenger_type: PassengerType, price: int):
-        for i, (type, _) in enumerate(self.__passenger_fare):
-            if type is passenger_type:
-                self.__passenger_fare[i] = (type, price)
-                break
-        else:
-            self.__passenger_fare.append(
-                (passenger_type, price)
-            )
-
-    def get_seat_price(self, seat_type: SeatType):
-        for type, price in self.__seat_fare:
-            if type is seat_type:
-                return price
-        raise KeyError
-    
-    def set_seat_price(self, seat_type: SeatType, price: int):
-        for i, (type, _) in enumerate(self.__seat_fare):
-            if type is seat_type:
-                self.__seat_fare[i] = (type, price)
-                break
-        else:
-            self.__seat_fare.append(
-                (seat_type, price)
-            )
-            
-    def get_price(self, 
-        passenger_type: PassengerType, 
-        seat_type: Optional[SeatType] = None
-    ):
-        """
-        if seat_type is None, assume that passenger is not assigned a seat (can be any seat)
-        so a fee for choosing a seat is not included
-        """
-        return sum([
-            self.get_passenger_price(passenger_type), 
-            self.get_seat_price(seat_type) if seat_type else 0
-        ])
-        
-    def pax_price(self, pax: Pax):
-        return sum(
-            number * self.get_price(passenger_type)
-            for passenger_type, number in pax
-        )
-
-
 @dataclass(slots=True)
 class FlightInstance:
     """
-    ### A derivative of Flight
+    A derivative from `Flight`, represent a process of flying 
+    that will be operated on a specific date and aircraft
+    
+        - date: `datetime.date`
+            - a date when this `FlightInstance` depart
+        - flight: `Flight`
+            - a template of this `FlightInstance`
+        - aircraft: `Aircraft`
+            - an aircraft that will be used for this `FlightInstance`
+            
+        - fares: `tuple[tuple[TravelClass, Fare], ...]`
+            - a parameter for initializing components
+            - represent a `Fare` for each travel class of this `FlightInstance`
+            
+        - components: `tuple[FlightClass, ...]`
+            - a tuple of `FlightClass` which each represent a travel class section of this `FlightInstance`
     """
     __date: dt.date # type: ignore
     __flight: Flight # type: ignore
     __aircraft: Aircraft # type: ignore
-    __fares: tuple[Fare] # type: ignore
+    fares: InitVar[fares_param]
     
-    __booking_record: list[FlightReservation] = field(init=False, default_factory=list)
+    __components: tuple[FlightClass, ...] = field(init=False)
     __status: FlightStatus = field(init=False, default=FlightStatus.SCHEDULED)
+    
+    def __post_init__(self, fares: fares_param):
+        self.__components = tuple(
+            FlightClass(self, *args) for args in fares
+        )
     
     @property
     def flight(self):
@@ -129,79 +90,132 @@ class FlightInstance:
         return self.__aircraft
     
     @property
-    def booking_record(self):
-        return self.__booking_record
-    
-    @property
     def status(self):
         return self.__status
+    
+    @property
+    def components(self):
+        return self.__components
     
     @property
     def designator(self):
         return self.flight.designator
     
     @property
-    def origin(self):
-        return self.flight.origin
-    
-    @property
-    def destination(self):
-        return self.flight.destination
-    
-    @property
     def all_travel_class(self):
         return {
-            cabin.travel_class for desk in self.aircraft.desks 
-            for cabin in desk.cabins
+            cabin.travel_class 
+            for cabin in self.aircraft.all_cabins
         }
         
-    def get_fare(self, travel_class: TravelClass):
-        for fare in self.__fares:
-            if fare.travel_class == travel_class:
-                return fare
+    def get_class(self, travel_class: TravelClass):
+        for component in self.components:
+            if component.travel_class is travel_class:
+                return component
+            
         raise KeyError
         
     def cancel(self):
         self.__status = FlightStatus.CANCELLED
-    
-    def get_all_comfirmed(self):
+        
+    def bookable(self, pax: Pax):
         """
-        get all confirmed reservations of this FlightInstance
+        is any travel class in this flight bookable
         """
-        return [
-            reservation for reservation in self.booking_record
-            if reservation.holder.status == BookingStatus.COMPLETED
-        ]
+        return any(
+            component.bookable(pax)
+            for component in self.components
+        )
     
-    def get_occupied_of(self, travel_class: TravelClass):
+
+@dataclass(slots=True)
+class FlightClass:
+    """
+        - One flight (instance) provides a service on different `TravelClass`, each `TravelClass` is independent of each other.
+        - Before customers book a flight they have to choose a `TravelClass` that is available, so they will not interfere with other `TravelClass`.
+        - `FlightClass` represent that one travel class service section.
+        - Most of the communication between customer and `FlightInstance` involve with specific `TravelClass`, 
+        it is convenient to sperate `FlightClass`, get them by `TravelClass` and use its method instead of passing `TravelClass` to every `FlightInstance` method.
+    """
+    __host: FlightInstance # type: ignore
+    __travel_class: TravelClass # type: ignore
+    __fare: Fare # type: ignore
+    __booking_record: list[FlightReservation] = field(init=False, default_factory=list)
+    
+    @property
+    def host(self):
+        return self.__host
+    
+    @property
+    def travel_class(self):
+        return self.__travel_class
+    
+    @property
+    def fare(self):
+        return self.__fare
+    
+    @fare.setter
+    def fare(self, value: Fare):
+        """
+        in case we want to update the price in this `Fare` which was shared with other flights (`FlightClass`), 
+        but we want to change only for this flight
+        """
+        self.__fare = value
+    
+    @property
+    def booking_record(self):
+        return self.__booking_record
+        
+    def get_comfirmed(self):
+        """
+        get all confirmed reservations
+        """
+        for reservation in self.booking_record:
+            if reservation.holder.status is BookingStatus.COMPLETED:
+                yield reservation
+        
+    def get_occupied_seats(self):
         """
         get all reserved seats that have be paid (reservation status is confirmed)
         """
         return {
-            seat for reservation in self.get_comfirmed_of(travel_class) if reservation.selected
-            for selected in reservation.selected if (seat := selected.seat) is not None
+            selected.seat for reservation in self.get_comfirmed()
+            for selected in reservation.selected if selected.seat
         }
         
-    def get_comfirmed_of(self, travel_class: TravelClass):
+    def get_remain_seats(self):
         """
-        get confirmed reservations that match travel_class of this FlightInstance
+        seats that have not been selected by any reservation
         """
-        return [
-            reservation for reservation in self.get_all_comfirmed() 
-            if reservation.travel_class == travel_class
-        ]
-
-    def bookable(self, pax: Pax, travel_class: TravelClass):
-        """
-        if specified travel class of this FlightInstance is bookable for pax (number of passenger)
-        """
-        seats_left = self.get_seats_left(travel_class)
-        return len(seats_left) >= pax.total_capable
+        all = self.host.aircraft.get_seats_of(self.travel_class)
+        occupied = self.get_occupied_seats()
+        return all - occupied
     
+    def get_seats_left(self):
+        """
+        not same as `get_occupied_seats`, it return as int which include a number of seats 
+        that may not specified position in reservation
+        """
+        all_seats = self.host.aircraft.get_seats_of(self.travel_class)
+        return len(all_seats) - sum(
+            len(reservation.selected) for reservation in self.get_comfirmed()
+        )
+        
+    def bookable(self, pax: Pax):
+        return self.get_seats_left() >= pax.total
+
     def booked(self, reservation: FlightReservation):
         self.booking_record.append(reservation)
-        
-    def get_seats_left(self, travel_class: TravelClass):
-        all = self.aircraft.get_seats_of(travel_class)
-        occupied = self.get_occupied_of(travel_class)
-        return all - occupied
+    
+"""
+ถ้าครอบครัวประกอบไปด้วยคนแต่ละคน
+กลุ่มคนประกอบไปด้วยแต่ละครอบครัว
+สหภาพที่ประกอบไปด้วยกลุ่มคน
+สหพันธ์ประกอบไปด้วยสหภาพ
+
+แต่ละการแบ่งนี้มีชุดการกระทำที่แตกต่างกัน 
+ครอบครัวก็มีชุดการกระทำของครอบครัว
+กลุ่มคนก็มีชุดการกระทำของกลุ่มคน
+แต่กลุ่มคนและครอบครัวมีคุณลักษณะบางอย่างร่วมกันเช่น จำนวนคน
+ดังนั้นในทุกชุดการแบ่งนี้ควรมี method จำนวนคน ของตนเอง?
+"""
