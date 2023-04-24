1d86bb2e6115d2a955dc0af0ab821272ec9355d9
Sun Chokun <63220735+shoguncoffee@users.noreply.github.com>
2023-04-24 11:04:55 +0700

    feat: add coordinates to Airport for calculating

--- app/src/airport.py
+++ app/src/airport.py
@@ -1,12 +1,15 @@
 from __future__ import annotations
 from .base import *
 
+from geopy.distance import great_circle
+
+
 @dataclass(slots=True, frozen=True)
 class Airport:
     __name: str # type: ignore
     __location_code: str # type: ignore
-    __city: str # type: ignore
     __country: str # type: ignore
+    __coordinates: tuple[float, float] # type: ignore
     
     @property
     def name(self):
@@ -15,11 +18,14 @@ class Airport:
     @property
     def location_code(self):
         return self.__location_code
-    
-    @property
-    def city(self):
-        return self.__city
-    
+
     @property
     def country(self):
         return self.__country
+
+    @property
+    def coordinates(self):
+        return self.__coordinates
+    
+    def distance(self, other: Self):
+        return great_circle(self.coordinates, other.coordinates)
